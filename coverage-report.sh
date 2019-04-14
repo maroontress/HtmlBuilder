72f892af0710798fb90899f16da52ad0d40a6e1f
Tomohisa Tanaka (Maroontress) <Tomohisa.Tanaka@gmail.com>
2019-04-15 00:54:20 +0900

    Update version 1.0.2 and dependencies. (#9)

--- coverage-report.sh
+++ coverage-report.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dotnet ~/.nuget/packages/reportgenerator/4.1.0/tools/netcoreapp2.0/ReportGenerator.dll --reports:HtmlBuilder.Test/coverage.opencover.xml --targetdir:Coverlet-html
+dotnet ~/.nuget/packages/reportgenerator/4.1.2/tools/netcoreapp2.0/ReportGenerator.dll --reports:HtmlBuilder.Test/coverage.opencover.xml --targetdir:Coverlet-html
