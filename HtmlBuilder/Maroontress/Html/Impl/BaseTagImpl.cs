939d01620eeb8a3b43da32aeb7cefe2c8416cd68
Tomohisa Tanaka (Maroontress) <Tomohisa.Tanaka@gmail.com>
2019-09-17 00:57:29 +0900

    Update dependencies. (#19)
    - Fix warnings.

--- HtmlBuilder/Maroontress/Html/Impl/BaseTagImpl.cs
+++ HtmlBuilder/Maroontress/Html/Impl/BaseTagImpl.cs
@@ -3,7 +3,6 @@ namespace Maroontress.Html.Impl
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using StyleChecker.Annotations;
 
     /// <summary>
     /// Abstraction of Tag and EmptyTag.
